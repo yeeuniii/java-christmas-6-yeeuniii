ca86f4f37b4f2cc7fb28d85534ffc5515a30ce75
yeeuniii <anna862700@gmail.com>
2023-11-14 03:46:23 +0900

    feat: 주간 할인 및 특별 할인 가격 구하기

--- src/main/java/christmas/Benefit.java
+++ src/main/java/christmas/Benefit.java
@@ -2,7 +2,9 @@ package christmas;
 
 public class Benefit {
     private static final int GIFT_EVENT_PRICE = 120000;
-    private Date date;
+    private static final int WEEK_EVENT_PRICE = 2023;
+    private static final int SPECIAL_EVENT_PRICE = 1000;
+    private final Date date;
 
     public Benefit(final Date date) {
         this.date = date;
@@ -15,6 +17,28 @@ public class Benefit {
     public int getChristmasDayDiscount() {
         if (date.isChristmasEvent()) {
             return 1000 + (date.getDay() - 1) * 100;
+        }
+        return 0;
+    }
+
+    private int calculateDiscount(final boolean isDiscountedDay, final int count) {
+        if (date.isEvent() && isDiscountedDay) {
+            return count * WEEK_EVENT_PRICE;
+        }
+        return 0;
+    }
+
+    public int getWeekdayDiscount(final int count) {
+        return calculateDiscount(!date.isWeekend(), count);
+    }
+
+    public int getWeekendDiscount(final int count) {
+        return calculateDiscount(date.isWeekend(), count);
+    }
+
+    public int getSpecialDiscount(final int count) {
+        if (date.isEvent()) {
+            return SPECIAL_EVENT_PRICE;
         }
         return 0;
     }
--- src/test/java/christmas/BenefitTest.java
+++ src/test/java/christmas/BenefitTest.java
@@ -19,4 +19,20 @@ public class BenefitTest {
         Benefit benefit = new Benefit(new Date("26"));
         assertThat(benefit.getChristmasDayDiscount()).isEqualTo(0);
     }
+
+    @DisplayName("평일인 경우 할인 가격 확인 및 주말 할인 적용 안됨")
+    @Test
+    void getWeekdayDiscount() {
+        Benefit benefit = new Benefit(new Date("31"));
+        assertThat(benefit.getWeekdayDiscount(1)).isEqualTo(2023);
+        assertThat(benefit.getWeekendDiscount(1)).isEqualTo(0);
+    }
+
+    @DisplayName("주말인 경우 할인 가격 확인 및 평일 할인 적용 안됨")
+    @Test
+    void getWeekendDiscount() {
+        Benefit benefit = new Benefit(new Date("30"));
+        assertThat(benefit.getWeekdayDiscount(1)).isEqualTo(0);
+        assertThat(benefit.getWeekendDiscount(1)).isEqualTo(2023);
+    }
 }
